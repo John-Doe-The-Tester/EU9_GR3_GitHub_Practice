cdeb54918e0f2dfb81e077b95d01f94e8f15699d
kenanevni <kenanevni@gmail.com>
2022-11-14 21:44:53 +0300

    new line

--- src/test/java/com/git_commands/kenan.java
+++ src/test/java/com/git_commands/kenan.java
@@ -3,5 +3,6 @@ package com.git_commands;
 public class kenan {
     public static void main(String[] args) {
         System.out.println("hello git world");
+        System.out.println("new line");
     }
 }
